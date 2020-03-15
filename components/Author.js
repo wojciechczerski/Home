5147e0b779c6f5420951cab79e20089a130a1620
Wojciech Czerski <wojciech.czerski@gmail.com>
2020-03-15 11:31:29 +0100

    Add index page with Author component

---
+++ components/Author.js
@@ -0,0 +1,8 @@
+const style = {
+  display: "flex",
+  justifyContent: "flex-end"
+};
+
+const Author = props => <div style={style}>{props.name}</div>;
+
+export default Author;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Author from "../components/Author";
+
+export default function Index() {
+  return (
+    <div>
+      <Author name="Wojciech Czerski" />
+    </div>
+  );
+}
