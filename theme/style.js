313ea1cfc977000c5187e2aa8b2a2edfaafc37e0
Wojciech Czerski <wojciech.czerski@gmail.com>
2020-03-22 18:26:27 +0100

    Set lighter italic font for body text

--- theme/style.js
+++ theme/style.js
@@ -1,6 +1,8 @@
 export const textStyle = {
   body: {
     color: "white",
+    fontWeight: "lighter",
+    fontStyle: "italic",
     fontSize: "1em",
     fontFamily: "Arial, Helvetica, sans-serif",
     lineHeight: "1.4"
